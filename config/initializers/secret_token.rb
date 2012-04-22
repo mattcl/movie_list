5bdd6469a5551bef7e6746973ccb5a0d1ad5a5cc
Matt Chun-Lum <mchunlum@gmail.com>
2012-04-21 22:25:45 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieList::Application.config.secret_token = 'e3f0b06f5100099cd4c170afd0455c78f42f0f19014e2f02c76b6252beef325e598b2dd05f1faeb741e87f3cd46e6627fd52ba2c21850f72428af1c642ca1354'
